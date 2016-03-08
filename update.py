51e0522627e55285372be35474317c12ffb82da7
Alex Thompson <godfoder@acis.ufl.edu>
2016-03-08 02:23:36 -0500

    update local repo to latest tagged release.

--- update.py
+++ update.py
@@ -1,10 +1,24 @@
 import sys
 
+import semver
+
 from dulwich import porcelain
 from dulwich.client import get_transport_and_path
 from dulwich.objectspec import parse_reftuples
 
 with porcelain.open_repo_closing(".") as repo:
+    latest_version = None
+    latest_version_ref = None
+
+    for ref in repo.refs.as_dict(b"refs/tags"):
+        if ref.startswith(b"v"):
+            sv_part = ref[1:].decode('ascii')
+            if latest_version is None:
+                latest_version = sv_part
+                latest_version_ref = repo.refs[b"refs/tags/" + ref]
+            elif semver.compare(sv_part, latest_version) > 0:
+                latest_version = sv_part
+                latest_version_ref = repo.refs[b"refs/tags/" + ref]
 
     client, host_path = get_transport_and_path("git://github.com/godfoder/dulwitch_selfref")
 
@@ -15,6 +29,18 @@ with porcelain.open_repo_closing(".") as repo:
 
     dirty = False
     for ref in remote_refs:
+        if ref.startswith(b"refs/tags/v"):
+            sv_part = ref[11:].decode('ascii')
+            if latest_version is None:
+                latest_version = sv_part
+                latest_version_ref = remote_refs.refs[ref]
+            elif semver.compare(sv_part, latest_version) > 0:
+                latest_version = sv_part
+                latest_version_ref = remote_refs.refs[ref]
+        elif ref == b"HEAD":            
+            continue # Skip remote HEAD (only update to latest tagged)
+
+        # Update Local Refs from remote
         if ref not in repo.refs:
             print("changed_ref", ref, remote_refs[ref])
             repo.refs[ref] = remote_refs[ref]
@@ -25,6 +51,10 @@ with porcelain.open_repo_closing(".") as repo:
             dirty = True
         else:
             pass
+
+    if repo.refs[b"HEAD"] != latest_version_ref:
+        repo.refs[b"HEAD"] = latest_version_ref
+        dirty = True
 
     if dirty:
         print("dirty index, syncing")
